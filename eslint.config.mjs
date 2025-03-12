377f4433722b14939873ffd6be55e129c93d4dca
m09nlight <ivan.portyanko1609@gmail.com>
2025-03-13 00:58:35 +0300

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,67 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintReact from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import eslintReactRefresh from 'eslint-plugin-react-refresh';
+// import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default tseslint.config(
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': eslintReactHooks,
+      react: eslintReact,
+      'react-refresh': eslintReactRefresh,
+      // prettier: prettierPlugin,
+    },
+  },
+  {
+    ignores: ['node_modules', 'build'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2022,
+        __IS_DEV__: true,
+      },
+      // parserOptions: eslintReact.configs.recommended.parserOptions,
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json'],
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    rules: {
+      // ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'prefer-const': 'warn',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      'no-empty-pattern': 'warn',
+    },
+  }
+);
+// export default [
+//   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+//   { languageOptions: { globals: globals.browser } },
+//   pluginJs.configs.recommended,
+//   {
+//     rules: {
+//       'no-unused-vars': 'warn',
+//       'no-undef': 'warn',
+//     },
+//   },
+//   ...tseslint.configs.recommended,
+//   eslintReact.configs.flat.recommended,
+// ];
