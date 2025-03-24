50088ad8c350ae4769f84f08d7131d7baca1880e
m09nlight <ivan.portyanko1609@gmail.com>
2025-03-25 01:28:35 +0300

    added reg cli

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -50,6 +50,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
       'no-empty-pattern': 'warn',
       // 'i18next/no-literal-string': [
       //   'error',
