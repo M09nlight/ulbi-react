c5df2177a57ea9684b312422e088b6c6ca844f29
m09nlight <ivan.portyanko1609@gmail.com>
2025-03-31 21:52:19 +0300

    add reducer manager and asyncReducers for stories

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,12 +1,24 @@
 import 'app/styles/index.scss';
 import { StoryFn } from '@storybook/react'; // Removed Story import
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+
+const defaultReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+  loginForm: loginReducer,
+};
 
 export const StoreDecorator =
-  (state: DeepPartial<StateSchema>) => (StoryComponent: StoryFn) => {
+  (
+    state: DeepPartial<StateSchema>,
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+  ) =>
+  (StoryComponent: StoryFn) => {
     return (
-      <StoreProvider initialState={state}>
+      <StoreProvider
+        initialState={state}
+        asyncReducers={{ ...defaultReducers, ...asyncReducers }}
+      >
         <StoryComponent />
       </StoreProvider>
     );
