77cf01b3f825b812d2001eb57861453c227dfcad
m09nlight <ivan.portyanko1609@gmail.com>
2025-04-05 13:09:11 +0300

    fix tests

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -44,30 +44,38 @@ describe('loginByUsername', () => {
   //   expect(result.payload).toEqual('error');
   // });
   test('success login', async () => {
-    mockedAxios.post.mockReturnValue(
+    const thunk = new TestAsyncThunk(loginByUsername);
+    // mockedAxios.post.mockReturnValue(
+    //   Promise.resolve({
+    //     data: userValue,
+    //   })
+    // );
+    thunk.api.post.mockReturnValue(
       Promise.resolve({
         data: userValue,
       })
-    );
-    const thunk = new TestAsyncThunk(loginByUsername);
+    ); //with extra arg by axios instance
     const result = await thunk.callThunk({ password: '123', username: '123' });
 
     expect(thunk.dispatch).toHaveBeenCalledWith(
       userActions.setAuthData(userValue)
     );
     expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-    expect(mockedAxios.post).toHaveBeenCalled();
+    // expect(mockedAxios.post).toHaveBeenCalled();
+    expect(thunk.api.post).toHaveBeenCalled(); //with extra arg by axios instance
     expect(result.meta.requestStatus).toBe('fulfilled');
     expect(result.payload).toEqual(userValue);
   });
   test('error login', async () => {
-    mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
-
     const thunk = new TestAsyncThunk(loginByUsername);
+    // mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+    thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 })); //with extra arg by axios instance
+
     const result = await thunk.callThunk({ password: '123', username: '123' });
 
     expect(thunk.dispatch).toHaveBeenCalledTimes(2);
-    expect(mockedAxios.post).toHaveBeenCalled();
+    // expect(mockedAxios.post).toHaveBeenCalled();
+    expect(thunk.api.post).toHaveBeenCalled(); //with extra arg by axios instance
     expect(result.meta.requestStatus).toBe('rejected');
     expect(result.payload).toEqual('error');
   });
--- src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -1,5 +1,6 @@
 import { AsyncThunkAction } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
+import axios, { AxiosStatic } from 'axios';
 
 type ActionCreatorType<Return, Arg, RejectValue> = (
   arg: Arg
@@ -10,20 +11,34 @@ type ActionCreatorType<Return, Arg, RejectValue> = (
     rejectValue: RejectValue;
   }
 >;
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios, true);
+
 export class TestAsyncThunk<Return, Arg, RejectValue> {
   dispatch: jest.MockedFn<any>;
   getState: () => StateSchema;
   actionCreator: ActionCreatorType<Return, Arg, RejectValue>;
+  api: jest.MockedFunctionDeep<AxiosStatic>; //with extra arg by axios instance
+  navigate: jest.MockedFn<any>; //with extra arg by axios instance
 
   constructor(actionCreator: ActionCreatorType<Return, Arg, RejectValue>) {
     this.actionCreator = actionCreator;
     this.dispatch = jest.fn();
     this.getState = jest.fn();
+
+    this.api = mockedAxios; //with extra arg by axios instance
+    this.navigate = jest.fn(); //with extra arg by axios instance
   }
 
   async callThunk(arg: Arg) {
     const action = this.actionCreator(arg);
-    const result = await action(this.dispatch, this.getState, undefined);
+    // const result = await action(this.dispatch, this.getState, undefined);
+    const result = await action(this.dispatch, this.getState, {
+      api: this.api,
+      navigate: this.navigate,
+    }); //with extra arg by axios instance
 
     return result;
   }
