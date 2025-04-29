acec45d0459a88d9c5b4324719e286682bf82cdc
m09nlight <ivan.portyanko1609@gmail.com>
2025-04-29 22:58:27 +0300

    skip tests, add stubs (fixtures)

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -9,5 +9,20 @@ Cypress.Commands.addAll(profileCommands);
 Cypress.Commands.addAll(articleCommands);
 Cypress.Commands.addAll(commentsCommands);
 Cypress.Commands.addAll(ratingCommands);
+// Cypress.Commands.overwrite('intercept', () => { // можно перезаписать и автоматизировать
+//
+//   const FIXTIRE_MODE = process.env.FIXTURE_MODE;
+//   const fixtureName = requestAnimationFrame.METHOD + req.url + hash(req.body);
+//   if (FIXTIRE_MODE === 'READ') {
+//     readFixture(fixtureName);
+//   }
+
+//   if (FIXTIRE_MODE === 'WRITE') {
+//     createFixture(fixtureName, req.body);
+//   }
+//   if (FIXTIRE_MODE === 'API') {
+
+//   }
+// });
 
 export {};
