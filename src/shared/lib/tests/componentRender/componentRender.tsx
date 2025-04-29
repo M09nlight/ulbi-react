d70751e760e6def5b58e1098e23e7700ad30b97b
m09nlight <ivan.portyanko1609@gmail.com>
2025-04-29 23:59:56 +0300

    add tests for isolated components with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,27 +1,52 @@
-import { ReactNode } from 'react';
+import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+import i18nForTests from '@/shared/config/i18n/i18nForTests';
+import { Theme } from '@/shared/consts/theme';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
-import i18nForTests from '@/shared/config/i18n/i18nForTests';
-import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
-import { ReducersMapObject } from '@reduxjs/toolkit';
+// eslint-disable-next-line eslint-plugin-check-path-m09blight-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+// eslint-disable-next-line eslint-plugin-check-path-m09blight-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 export interface componentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
+}
+
+interface TestProviderProps {
+  children: ReactNode;
+  options: componentRenderOptions;
+}
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+  const {
+    route = '/',
+    initialState,
+    asyncReducers,
+    theme = Theme.LIGHT,
+  } = options;
+
+  return (
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
+        <I18nextProvider i18n={i18nForTests}>
+          <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>{children}</div>
+          </ThemeProvider>
+        </I18nextProvider>
+      </StoreProvider>
+    </MemoryRouter>
+  );
 }
 
 export const componentRender = (
   component: ReactNode,
   options: componentRenderOptions = {}
 ) => {
-  const { route = '/', initialState, asyncReducers } = options;
-  return render(
-    <MemoryRouter initialEntries={[route]}>
-      <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
-        <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-      </StoreProvider>
-    </MemoryRouter>
-  );
+  return render(<TestProvider options={options}>{component}</TestProvider>);
 };
