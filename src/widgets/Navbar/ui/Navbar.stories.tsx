4deaa0a2e3e624bf34cc73559133304bdde6114e
m09nlight <ivan.portyanko1609@gmail.com>
2025-05-03 00:45:56 +0300

    redesign auth modal, micro fixes

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
-import Navbar from './Navbar';
+import { Navbar } from './Navbar';
 import { Theme } from '@/shared/consts/theme';
 
 export default {
